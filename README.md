bac484d35c36232d4c5eae685d581692664185a3
kyle java <42562960+kylejava@users.noreply.github.com>
2021-02-24 21:57:36 -0800

    Need to add test case

---
+++ README.md
@@ -0,0 +1,11 @@
+# Intro To Flask Workshop for IEEE
+
+This workshop helps students learn the basics to Flask. Students will clone this repository with the front-end code
+and will create the back-end through the workshop
+
+## Tools to install
+- Python
+- Pip
+- Flask (pip install flask)
+
+~Note: Will upload main.py after workshop
--- main.py
+++ main.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/user' , methods = ['GET', 'POST'])
+def user():
+    if(request.method == 'POST'):
+        name = request.form['name']
+
+    return render_template('user.html', name = name)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
