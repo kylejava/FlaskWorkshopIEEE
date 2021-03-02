d59e26f55e64837a6fcc30ff98c4100039ac5aa2
kyle java <42562960+kylejava@users.noreply.github.com>
2021-03-02 12:40:07 -0800

    Finished workshop

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from flask import Flask, render_template, flash, request
 
 app = Flask(__name__)
 
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+@app.route('/user' , methods = ['GET', 'POST'])
+def user():
+    if(request.method == 'POST'):
+        name = request.form['name']
+
+    return render_template('user.html', name = name)
 
 
 if __name__ == "__main__":
